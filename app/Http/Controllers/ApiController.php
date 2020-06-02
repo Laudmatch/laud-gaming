61785e7e7e8de1af03379453fbdf96365bc4b9e7
Laudmatch <egorka.arkov@mail.ru>
2020-06-02 14:02:41 +0300

    popka

---
+++ app/Http/Controllers/ApiController.php
@@ -0,0 +1,129 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ApiController extends Controller
+{
+ 
+{
+    public function gonki(Request $request)
+    {
+        return view('gonki');
+    }
+    public function horror(Request $request)
+    {
+        return view('horror');
+    }
+    public function index(Request $request)
+    {
+        return view('index');
+    }
+    public function korzina(Request $request)
+    {
+        return view('korzina');
+    }
+    public function mmo(Request $request)
+    {
+        return view('mmo');
+    }
+    public function novinki(Request $request)
+    {
+        return view('novinki');
+    }
+    public function shooters(Request $request)
+    {
+        return view('shooters');
+    }
+    public function skidki(Request $request)
+    {
+        return view('skidki');
+    }
+    public function sport(Request $request)
+    {
+        return view('sport');
+    }
+	public function t1(Request $request)
+    {
+        return view('t1');
+    }
+	public function t2(Request $request)
+    {
+        return view('t2');
+    }
+	public function t3(Request $request)
+    {
+        return view('t3');
+    }
+	public function t4(Request $request)
+    {
+        return view('t4');
+    }
+	public function t5(Request $request)
+    {
+        return view('t5');
+    }
+	public function t6(Request $request)
+    {
+        return view('t6');
+    }
+	public function t7(Request $request)
+    {
+        return view('t7');
+    }
+	public function t8(Request $request)
+    {
+        return view('t8');
+    }
+	public function t9(Request $request)
+    {
+        return view('t9');
+    }
+	public function t10(Request $request)
+    {
+        return view('t10');
+    }
+	public function t11(Request $request)
+    {
+        return view('t11');
+    }
+	public function t12(Request $request)
+    {
+        return view('t12');
+    }
+	public function t13(Request $request)
+    {
+        return view('t13');
+    }
+	public function t14(Request $request)
+    {
+        return view('t14');
+    }
+	public function t15(Request $request)
+    {
+        return view('t15');
+    }
+	public function t16(Request $request)
+    {
+        return view('t16');
+    }
+	public function t17(Request $request)
+    {
+        return view('t17');
+    }
+	public function t18(Request $request)
+    {
+        return view('t18');
+    }
+	public function t19(Request $request)
+    {
+        return view('t19');
+    }
+	public function t20(Request $request)
+    {
+        return view('t20');
+    }
+	
+}
+}
--- routes/web.php
+++ routes/web.php
@@ -12,92 +12,33 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-
-Route::get('/', function () {
-    return view('index');
-});
-Route::get('/gonki', function () {
-    return view('gonki');
-});
-Route::get('/horror', function () {
-    return view('horror');
-});
-Route::get('/korzina', function () {
-    return view('korzina');
-});
-Route::get('/mmo', function () {
-    return view('mmo');
-});
-Route::get('/novinki', function () {
-    return view('novinki');
-});
-Route::get('/shooters', function () {
-    return view('shooters');
-});
-Route::get('/skidki', function () {
-    return view('skidki');
-});
-Route::get('/sport', function () {
-    return view('sport');
-});
-Route::get('/t1', function () {
-    return view('t1');
-});
-Route::get('/t2', function () {
-    return view('t2');
-});
-Route::get('/t3', function () {
-    return view('t3');
-});
-Route::get('/t4', function () {
-    return view('t4');
-});
-Route::get('/t5', function () {
-    return view('t5');
-});
-Route::get('/t6', function () {
-    return view('t6');
-});
-Route::get('/t7', function () {
-    return view('t7');
-});
-Route::get('/t8', function () {
-    return view('t8');
-});
-Route::get('/t9', function () {
-    return view('t9');
-});
-Route::get('/t10', function () {
-    return view('t10');
-});
-Route::get('/t11', function () {
-    return view('t11');
-});
-Route::get('/t12', function () {
-    return view('t12');
-});
-Route::get('/t13', function () {
-    return view('t13');
-});
-Route::get('/t14', function () {
-    return view('t14');
-});
-Route::get('/t15', function () {
-    return view('t15');
-});
-Route::get('/t16', function () {
-    return view('t16');
-});
-Route::get('/t17', function () {
-    return view('t17');
-});
-Route::get('/t18', function () {
-    return view('t18');
-});
-Route::get('/t19', function () {
-    return view('t19');
-});
-Route::get('/t20', function () {
-    return view('t20');
-});
-
+Route::get('/', 'ApiController@index');
+Route::get('/gonki', 'ApiController@gonki');
+Route::get('/horror', 'ApiController@horror');
+Route::get('/korzina', 'ApiController@korzina');
+Route::get('/mmo', 'ApiController@mmo');
+Route::get('/novinki', 'ApiController@novinki');
+Route::get('/shooters', 'ApiController@shooters');
+Route::get('/skidki', 'ApiController@skidki');
+Route::get('/sport', 'ApiController@sport');
+Route::get('/t1', 'ApiController@t1');
+Route::get('/t2', 'ApiController@t2');
+Route::get('/t3', 'ApiController@t3');
+Route::get('/t4', 'ApiController@t4');
+Route::get('/t5', 'ApiController@t5');
+Route::get('/t6', 'ApiController@t6');
+Route::get('/t7', 'ApiController@t7');
+Route::get('/t8', 'ApiController@t8');
+Route::get('/t9', 'ApiController@t9');
+Route::get('/t10', 'ApiController@t10');
+Route::get('/t11', 'ApiController@t11');
+Route::get('/t12', 'ApiController@t12');
+Route::get('/t13', 'ApiController@t13');
+Route::get('/t14', 'ApiController@t14');
+Route::get('/t15', 'ApiController@t15');
+Route::get('/t16', 'ApiController@t16');
+Route::get('/t17', 'ApiController@t17');
+Route::get('/t18', 'ApiController@t18');
+Route::get('/t19', 'ApiController@t19');
+Route::get('/t20', 'ApiController@t20');
+  
